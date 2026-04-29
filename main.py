cbcf5913497c81fd6278e3b6bcedb2e2ac52a3fa
lowestate <81164195+lowestate@users.noreply.github.com>
2026-04-29 15:31:15 +0300

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+dotenv_path = os.path.join(base_dir, '.env')
+load_dotenv(dotenv_path)
+
 from app.api.routes import router 
 
 app = FastAPI(title="AI Analyst API")
@@ -17,7 +23,7 @@ app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.main:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         #reload=True
